e9fc067377ee93f736320ece99ca34fef0d5deff
nikita <novikov_nv@icloud.com>
2024-03-26 15:21:41 +0300

    add FloatingActionButton

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -4,10 +4,17 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
@@ -19,6 +26,20 @@ fun HomeView() {
                     Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                shape = ShapeDefaults.ExtraLarge,
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                onClick = { /* TODO Add navigation to add screen*/
+                    Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+                }
+            )
+            {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
         }
     ) {
         LazyColumn(
