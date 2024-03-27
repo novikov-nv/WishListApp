019297ccda3559d655dfe2db8af11b60a43d8412
nikita <novikov_nv@icloud.com>
2024-03-27 15:59:28 +0300

    add WishDao

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -38,7 +38,8 @@ fun AddEditDetailView(
             AppBarView(
                 title =
                 if (id != 0L) stringResource(id = R.string.update_wish)
-                else stringResource(id = R.string.add_wish)
+                else stringResource(id = R.string.add_wish),
+                onBackNavClicked = {navController.navigateUp()}
             )
         }
     )
