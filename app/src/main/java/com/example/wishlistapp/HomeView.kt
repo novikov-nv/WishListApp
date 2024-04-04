7c7eeb7bf66c55c6a74ee2c125bc7ed95ffa26f1
nikita <novikov_nv@icloud.com>
2024-04-04 10:56:18 +0300

    add Wish with viewModel

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -57,13 +58,14 @@ fun HomeView(
             }
         }
     ) {
+        val wishlist = viewModel.getAllWishes.collectAsState(initial = listOf())
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
         )
         {
-            items(DummyWish.wishList){ wish ->
+            items(wishlist.value){ wish ->
                 WishItem(wish = wish) {
                 }
             }
