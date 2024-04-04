6c0f79c489c8ec45b7b3e6765398160136a1ac3d
nikita <novikov_nv@icloud.com>
2024-04-04 14:28:15 +0300

    ..

--- app/src/main/java/com/example/wishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/wishlistapp/MainActivity.kt
@@ -12,10 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wishlistapp.ui.theme.WishListAppTheme
 
-
-//TODO 193. День 13 - HomeView и Scaffold !!!!
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
